4f0d5d8c6ea4f7f5aa3a6033a15217f8fd6b4aa2
amar <ustavdica.private@gmail.com>
2024-12-06 09:02:39 +0100

    Correct hash function, according to Ramakrishnans book

--- src/lib/LinearHashtable.ts
+++ src/lib/LinearHashtable.ts
@@ -1,18 +1,22 @@
 
 
 
-
-
-
-
 class LinearHashtable {
 
+	private level: number;				// Current round number
+	private next: number;					// Next bucket to split
+	private N: number;						// Number of buckets
+	private bucketSize: number;
 
+	constructor(N = 2, bucketSize = 4) {
+		this.level = 0;
+		this.next = 0;
+		this.N = N;
+		this.bucketSize = bucketSize;
+	}
 
-
-
-
-
-
+	private hash(key: number) {
+		return key % (Math.pow(2, this.level) * this.N);
+	}
 
 }
