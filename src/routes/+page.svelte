0a9a412f89ecb3729e927b9ea1682c29257cc9fc
amar <ustavdica.private@gmail.com>
2024-12-06 10:08:51 +0100

    This might be it, looks like it is working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,36 @@
 <script lang="ts">
-	import { LinearHashtable } from '$lib/hashlin';
+	import { LinearHashtable } from '$lib/LinearHashtable';
 
-	// Initialize the hashtable
-	const table = new LinearHashtable(2);
+	const table = new LinearHashtable();
 
-	let newKey: number = $state(0);
-
-	let primaryPages: number[][] = $state([]);
-	table.primaryPages.subscribe((pages) => (primaryPages = pages));
-
-
-	const insertKey = (): void => {
-		table.insert(newKey);
-		newKey = 0; // Reset input field
-	};
+	let buckets = $state(table.getBuckets());
+	let key = $state(0);
 </script>
 
 <div class="flex flex-col gap-8 items-center justify-center w-screen h-screen bg-zinc-200">
 
 	<!-- For inserting new key -->
 	<div>
-		<input type="number"
-					 bind:value={newKey}
-					 placeholder="Enter a key"
-					 class="border border-gray-300 p-2 rounded"
+		<input
+			type="number"
+			onkeydown={(event: KeyboardEvent) => {
+				if (event.key === 'Enter') {
+					table.insert(key);
+					key = 0; // Reset the input field after insertion
+				}
+			}}
+			bind:value={key}
+			placeholder="Enter a key"
+			class="border border-gray-300 p-2 rounded"
 		>
 		<button
 			class="px-3 py-2 border border-gray-300 rounded"
-			onclick={insertKey}
+			onclick={() => table.insert(key)}
 		>insert
 		</button>
 	</div>
 
-	<table class="min-w-96">
-		<thead>
-		<tr>
-			<th>Hash Functions</th>
-			<th>Primary Pages</th>
-			<th>Overflow Pages</th>
-		</tr>
-		</thead>
-		<tbody>
-		{#each primaryPages as bucket}
-			<tr>
-				<td></td>
-				<td>
-					<div class="flex items-center justify-start px-2 gap-2 h-8 min-w-36 border border-gray-300 rounded">
-						{#each bucket as item }
-							<span class="flex items-center justify-center px-2">
-								{item}
-							</span>
-						{/each}
-					</div>
-				</td>
-				<td>
-
-				</td>
-			</tr>
-		{/each}
-		</tbody>
-	</table>
-
+	{#each buckets as bucket}
+		{bucket.toString()}
+	{/each}
 </div>
